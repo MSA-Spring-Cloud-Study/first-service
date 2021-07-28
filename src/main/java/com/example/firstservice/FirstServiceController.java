871196fe794f2064c58862233ad4ae1a7f0a401e
Luke(김기정) <kkj@gabia.com>
2021-07-28 16:37:45 +0900

    Init Project

---
+++ src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,28 @@
+package com.example.firstservice;
+
+import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/first-service/")
+@Slf4j
+public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
+    @GetMapping("/check")
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First Service %s", env.getProperty("local.server.port"));
+    }
+}
